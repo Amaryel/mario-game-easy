2af8b652931788a26b162ee195b9b1341eb84783
unknown <CTI@CTIPICOS37807.mppi.mp.br>
2024-02-19 08:11:58 -0300

    First Commit

---
+++ js/scrypt.js
@@ -0,0 +1,8 @@
+const mario = document.querySelector('.mario');
+const jump = () {
+
+    mario.classList.add('jump');
+    setTimeout(function, 500ms;
+}
+
+document.addEventListener('keydown' , jump);
